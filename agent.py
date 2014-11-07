dd7b63449061352b985c740d0ac62a610024f4ca
Manfred Touron <m@42.am>
2014-11-08 00:59:52 +0100

    Fetching configuration from metadata, subscribing to multiple queues

--- agent.py
+++ agent.py
@@ -1,38 +1,42 @@
 import json
 import os
+import re
 import redis
 import shlex
 import sys
 import urllib
+import urllib2
 from subprocess import Popen, PIPE, STDOUT, check_output
 
 from celery import Celery
 
 
-metadata = json.loads(urllib.urlopen(
-    'http://169.254.42.42/conf?format=json'
-).read())
+celery = Celery('tasks') #, broker=amqp, backend=amqp)
 
-# Configuration
-amqp_user = 'guest:guest'
-backend_host = '127.0.0.1'
-for tag in [tag.split('=') for tag in metadata.get('tags', [])]:
-    if tag[0] == 'manager':
-        backend_host = tag[1]
-    if tag[0] == 'amqp-user':
-        amqp_user = tag[1]
-amqp_user = os.environ.get('AMQP_USER', amqp_user)
-backend_host = os.environ.get('MASTER', backend_host)
 
-# Backends
-amqp = 'amqp://{}@{}:5672'.format(amqp_user, backend_host)
-celery = Celery('tasks', broker=amqp, backend=amqp)
-redis_instance = redis.Redis(host=backend_host)
+def get_metadata():
+    try:
+        return json.loads(urllib2.urlopen(
+            'http://169.254.42.42/conf?format=json',
+            None,
+            3,
+        ).read())
+    except:
+        return {'tags': []}
+
+
+def get_external_ip():
+    site = urllib.urlopen("http://checkip.dyndns.org/").read()
+    grab = re.findall('([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', site)
+    address = grab[0]
+    return address
+
 
 # run_command Task
 @celery.task(shared=True, time_limit=60)
 def run_command(command):
     task_id = run_command.request.id
+    lines = []
     proc = Popen(
         ['/bin/bash', '-c', command],
         stdout=PIPE, stderr=STDOUT,
@@ -45,6 +49,7 @@ def run_command(command):
         line = proc.stdout.readline()
         if line:
             print(line)
+            lines.append(line)
             redis_instance.publish('task:{}:stdout'.format(task_id),
                                    line.rstrip())
         else:
@@ -54,6 +59,40 @@ def run_command(command):
             break
     ret = {
         'retcode': proc.returncode,
+        'output': lines,
     }
     print(ret)
     return ret
+
+
+def main():
+    # Configuration
+    amqp_user = 'guest:guest'
+    backend_host = '127.0.0.1'
+
+    metadata = get_metadata()
+    metadata_queues = []
+    for tag in [tag.split('=') for tag in metadata.get('tags', [])]:
+        if tag[0] == 'manager':
+            backend_host = tag[1]
+        if tag[0] == 'amqp-user':
+            amqp_user = tag[1]
+        if tag[0] == 'queues':
+            metadata_queues = tag[1].split('|')
+
+    amqp_user = os.environ.get('AMQP_USER', amqp_user)
+    backend_host = os.environ.get('MASTER', backend_host)
+
+    # Backends
+    amqp = 'amqp://{}@{}:5672'.format(amqp_user, backend_host)
+
+    queues = ['celery', get_external_ip()] + metadata_queues
+
+    celery.conf['BROKER_URL'] = amqp
+                    
+    celery.select_queues(queues)
+
+    redis_instance = redis.Redis(host=backend_host)
+
+
+main()
